a80d399a31beafe01fa0ff645674f460f6e83c11
HinataAoki <sunreverse.0311.blue@gmail.com>
2021-09-16 22:08:00 +0900

    Add movie to text

--- MovieToLabel/utils.py
+++ MovieToLabel/utils.py
@@ -2,6 +2,8 @@
 # import matplotlib.pyplot as plt
 
 import io
+import os
+import glob
 import scipy.misc
 import numpy as np
 from six import BytesIO
@@ -47,3 +49,144 @@ def get_keypoint_tuples(eval_config):
   for edge in kp_list:
     tuple_list.append((edge.start, edge.end))
   return tuple_list
+
+def get_model_detection_function(model):
+  """Get a tf.function for detection."""
+
+  @tf.function
+  def detect_fn(image):
+    """Detect objects in image."""
+
+    image, shapes = model.preprocess(image)
+    prediction_dict = model.predict(image, shapes)
+    detections = model.postprocess(prediction_dict, shapes)
+
+    return detections, prediction_dict, tf.reshape(shapes, [-1])
+
+  return detect_fn
+
+
+def label_out(classes, scores, label_map_dict ,threshold=0.5):
+  output = []
+  for i in range(len(classes)):
+    if scores[i] > threshold:
+      keys = [k for k, v in label_map_dict.items() if v == classes[i]]
+      print(keys)
+      output+=keys
+    else:
+      break
+  return output
+
+
+def save_all_frames(video_path, dir_path, basename, ext='jpg'):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return
+
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
+
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+    n = 0
+
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            if n%20 == 0:
+                tn = n//20
+                cv2.imwrite('{}_{}.{}'.format(base_path, str(tn).zfill(digit), ext), frame)
+            n += 1
+        else:
+            return
+
+
+def movie_to_label(image_path, threshold=0.5):
+  #構成情報ファイルのパス。リポジトリにConfigファイルの揃ったフォルダがあるけど、微妙にモデル名が省略されていたりするので、ダウンロードしたものの方が確実？
+  pipeline_config = "./centernet_hg104_512x512_coco17_tpu-8/pipeline.config"
+  #チェックポイントのパス
+  model_dir = "./centernet_hg104_512x512_coco17_tpu-8/checkpoint"
+
+  #モデル構成情報読み込み
+  configs = config_util.get_configs_from_pipeline_file(pipeline_config)
+  model_config = configs['model']
+
+  #読み込んだ構成情報でモデルをビルド
+  detection_model = model_builder.build(
+        model_config=model_config, is_training=False)
+
+  #チェックポイントから重みを復元
+  ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+  ckpt.restore(os.path.join(model_dir, 'ckpt-0')).expect_partial()
+  detect_fn = get_model_detection_function(detection_model)
+
+  label_map_path = './models/research/object_detection/data/mscoco_label_map.pbtxt'
+  label_map = label_map_util.load_labelmap(label_map_path)
+  categories = label_map_util.convert_label_map_to_categories(
+      label_map,
+      max_num_classes=label_map_util.get_max_label_map_index(label_map),
+      use_display_name=True)
+  category_index = label_map_util.create_category_index(categories)
+  label_map_dict = label_map_util.get_label_map_dict(label_map, use_display_name=True)
+
+  # image_dir = '/content/drive/MyDrive/pjSOMA/バナナの画像/'
+  # image_path = os.path.join(image_dir, 'バナナの画像_000.jpg')
+  image_np = load_image_into_numpy_array(image_path)
+
+  # Things to try:
+  # Flip horizontally
+  # image_np = np.fliplr(image_np).copy()
+
+  # Convert image to grayscale
+  # image_np = np.tile(
+  #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+  input_tensor = tf.convert_to_tensor(
+      np.expand_dims(image_np, 0), dtype=tf.float32)
+  detections, predictions_dict, shapes = detect_fn(input_tensor)
+
+  label_id_offset = 1
+  image_np_with_detections = image_np.copy()
+
+  # Use keypoints if available in detections
+  keypoints, keypoint_scores = None, None
+  if 'detection_keypoints' in detections:
+    keypoints = detections['detection_keypoints'][0].numpy()
+    keypoint_scores = detections['detection_keypoint_scores'][0].numpy()
+
+  #ラベルを出力
+  return label_out((detections['detection_classes'][0].numpy() + label_id_offset).astype(int),detections['detection_scores'][0].numpy(), label_map_dict, threshold)
+
+  '''
+  viz_utils.visualize_boxes_and_labels_on_image_array(
+        image_np_with_detections,
+        detections['detection_boxes'][0].numpy(),
+        (detections['detection_classes'][0].numpy() + label_id_offset).astype(int),
+        detections['detection_scores'][0].numpy(),
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=200,
+        min_score_thresh=.30,
+        agnostic_mode=False,
+        keypoints=keypoints,
+        keypoint_scores=keypoint_scores,
+        keypoint_edges=get_keypoint_tuples(configs['eval_config']))
+
+  print(box_to_display_str_map)
+
+  plt.figure(figsize=(96,128))
+  plt.imshow(image_np_with_detections)
+  plt.show()
+  '''
+
+
+def movie_to_text(movie_path, img_dir="temp", img_name="temp", threthold=0.5):
+  print("Start save frames...")
+  save_all_frames(movie_path, img_dir, img_name)
+  frames = glob.glob(img_dir + "/*.jpg")
+  output = []
+  for i in range(len(frames)):
+    print(f"[{str(i)}/{str(len(frames))}] Start...")
+    output += movie_to_label(frames[i])
+  return list(set(output))
--- main.py
+++ main.py
@@ -6,24 +6,30 @@ from googletrans import Translator
 
 parser = argparse.ArgumentParser(description='ReCreation GAN') 
 parser.add_argument('-mp', '--movie_path', help='path of movie', default='temp.mp4')
-parser.add_argument('-vp', '--voice_path', help='path of movie', default='temp.wav')
+parser.add_argument('-vp', '--voice_path', help='path of voice', default='temp.wav')
+parser.add_argument('-th', '--threthold', help='threthold', default='0.5')
+parser.add_argument('--img_dir', help='path of folder saving temp imgs', default='temp')
+parser.add_argument('--img_name', help='name of saving temp imgs', default='temp')
 args = parser.parse_args()
 
 movie_path = args.movie_path
 voice_path = args.voice_path
+img_dir = args.img_dir
+img_name = args.img_name
+threthold = float(args.threthold)
 
 # 動画を作成する
 record.make_record(path=movie_path)
 
 # 動画から状況を説明するテキストを生成
-generated_text_from_movie = ml_utils.make_text_from_movie(movie_path)
+generated_text_from_movie = ml_utils.movie_to_text(movie_path, img_dir="temp", img_name="temp", threthold=0.5)
 
 # Speech to Text
 generated_text_from_speech = sr_utils.voice_to_text(movie_path, voice_path)
 
 # Translate to English
 translator = Translator()
-translated_speech_text = translator.translate(generated_text_from_speech, dest="ja");
+translated_speech_text = translator.translate(generated_text_from_speech, dest="ja")
 
 # テキストから画像を生成
 #generate_image_from_text(generated_text)
