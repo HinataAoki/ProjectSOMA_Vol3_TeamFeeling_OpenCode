4f1db6338d992c051ef201ef815bfeb3691f58ca
HinataAoki <sunreverse.0311.blue@gmail.com>
2021-09-17 23:45:21 +0900

    add gc

--- temp.py
+++ temp.py
@@ -1,4 +1,3 @@
-import os
 import gc
 import argparse
 from Record import record
@@ -7,25 +6,24 @@ from SpeechToLabel import utils as sl_utils
 from TextToImage import utils as ti_utils
 from googletrans import Translator
 
-parser = argparse.ArgumentParser(description='ReCreation GAN') 
-parser.add_argument('-mp', '--movie_path', help='path of movie', default='temp.mp4')
-parser.add_argument('-vp', '--voice_path', help='path of voice', default='temp.wav')
-parser.add_argument('-th', '--threthold', help='threthold', default='0.5')
-parser.add_argument('--img_dir', help='path of folder saving temp imgs', default='temp')
-parser.add_argument('--img_name', help='name of saving temp imgs', default='temp')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='ReCreation GAN') 
+# parser.add_argument('-mp', '--movie_path', help='path of movie', default='temp.mp4')
+# parser.add_argument('-vp', '--voice_path', help='path of voice', default='temp.wav')
+# parser.add_argument('-th', '--threthold', help='threthold', default='0.5')
+# parser.add_argument('--img_dir', help='path of folder saving temp imgs', default='temp')
+# parser.add_argument('--img_name', help='name of saving temp imgs', default='temp')
+# args = parser.parse_args()
 
-movie_path = args.movie_path
-voice_path = args.voice_path
-img_dir = args.img_dir
-img_name = args.img_name
-threthold = float(args.threthold)
+# movie_path = args.movie_path
+# voice_path = args.voice_path
+# img_dir = args.img_dir
+# img_name = args.img_name
+# threthold = float(args.threthold)
 
-# 動画を作成する
-# print("Start Recording...")
-# record.make_record(path=movie_path)
-
-if os.path.exists(movie_path):
+def make_text_from_movie(movie_path, voice_path, img_dir, img_name, threthold):
+    # 動画を作成する
+    print("Start Recording...")
+    # record.make_record(path=movie_path)
 
     # 動画から状況を説明するテキストを生成
     print("Start making text from movie...")
@@ -54,7 +52,7 @@ if os.path.exists(movie_path):
 
     gc.collect()
 
-    # テキストから画像を生成
-    ti_utils.generate_image_from_text(input_text)
-else:
-    print("Movie does not exist!")
+    return input_text
+
+# テキストから画像を生成
+# ti_utils.generate_image_from_text(input_text)
